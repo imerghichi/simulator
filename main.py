1355d6101802a8a2c8ce488820ed8ea5e1303c6a
Imane Merghichi <47861134+imerghichi@users.noreply.github.com>
2022-06-30 11:19:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,3 +26,25 @@ async def test(request:Request):
 async def test():
 	return {"data": "7888"}
 
+@app.post("/deliverActCode")
+async def deliverActCode():
+    return {"responseInfo": {
+   "responseUID": "user0016-lgkj-7777-7777-123456789aze",
+   "resultID": "ProceedWithSuccess",
+   "errorCode": "00000",
+   "errorDescription": "ProceedWithSuccess"
+},
+    "stepUpMethodExpirationTime": "2010-02-08T11:04:50.001Z",
+    "activationCodeLength": "10"
+}
+
+@app.post("/verifyActivationCode")
+async def deliverActCode():
+    return {"responseInfo": {
+   "responseUID": "user0016-lgkj-7777-7777-123456789aze",
+   "resultID": "ProceedWithSuccess",
+   "errorCode": "00000",
+   "errorDescription": "ProceedWithSuccess"
+},
+  "verificationResult": "1"
+}
