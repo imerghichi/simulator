baf26d042139b83224a3c7de4d9abd9e16c6ee92
Imane Merghichi <47861134+imerghichi@users.noreply.github.com>
2022-06-30 11:23:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse
-import json
-import os
+from fastapi import FastAPI
+
 app = FastAPI()
 
 @app.get("/")
 async def Home():
     return RedirectResponse("/docs")
-
-@app.post("/test")
-async def test(request:Request):
-   item = await request.json()
-   data1 = json.dumps(item["data"])
-   data2 = json.loads(data1)
-   f = open("main.py", "a")
-   #f.write("from fastapi import FastAPI\napp = FastAPI()\n")
-   f.write("\n@app."+item["verb"]+'("/'+item["url"]+'")\n')
-   f.write("\nasync def "+ item["function"]+"():\n")
-   f.write("\treturn " + data1+"\n\n")
-   f.close()
-   return data2
 
 @app.post("/testurl")
 
